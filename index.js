0122fe2664149382c0acee79c566dc61de43d9fb
gabrielacorbariufpr <165803154+gabrielacorbariufpr@users.noreply.github.com>
2025-05-19 12:11:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PostEvaluation from "./PostEvaluation";
+import PostEvaluation from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<PostEvaluation />);
+root.render(<App.js />);
