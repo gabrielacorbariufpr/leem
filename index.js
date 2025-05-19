5992c50e4924669ffa5df1dca64b1c20574bc0b6
gabrielacorbariufpr <165803154+gabrielacorbariufpr@users.noreply.github.com>
2025-05-19 12:08:39 -0300

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PostEvaluation from "./PostEvaluation";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<PostEvaluation />);
