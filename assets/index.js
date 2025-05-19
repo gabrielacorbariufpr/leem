f16fe0a65b1ae0c7aaabce5ba38fd38e0cbf7dbb
gabrielacorbariufpr <165803154+gabrielacorbariufpr@users.noreply.github.com>
2025-05-19 16:32:53 -0300

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+document.getElementById('root').innerHTML = `
+  <h1 style='font-family: sans-serif;'>🎓 Missão Final LEEM</h1>
+  <p>Bem-vindo à sua jornada de avaliação da experiência! Este é um exemplo publicado no GitHub Pages.</p>
+`;
